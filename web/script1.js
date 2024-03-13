ec5cf9cd949338e5d9886d1c97c9b2dc9cf54363
RedTy_YT <111154398+redtyyt@users.noreply.github.com>
2024-03-13 20:05:41 +0100

    Commit 4
    aggiunte diverse cose e molti altri documenti; linguaggio aggiunto: JAVASCRIPT e Node

---
+++ web/script1.js
@@ -0,0 +1,5 @@
+const downloadButton = document.getElementById("but1");
+
+downloadButton.addEventListener('click', function() {
+    window.location.href = "https://praga-monumentiapp.vercel.app/download.html";
+})
